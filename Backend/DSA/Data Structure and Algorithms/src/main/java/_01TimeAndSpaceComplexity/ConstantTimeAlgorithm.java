e7393405d2d331744fffdbcfaf6164b5e77acb40
PranaySingh13 <pranay.singh.1393@gmail.com>
2026-01-07 20:58:28 +0530

    ConstantTimeAlgorithm in Time and Space Complexity in DSA
    Signed-off-by: PranaySingh13 <pranay.singh.1393@gmail.com>

--- Backend/DSA/Data Structure and Algorithms/src/main/java/_01TimeAndSpaceComplexity/ConstantTimeAlgorithm.java
+++ Backend/DSA/Data Structure and Algorithms/src/main/java/_01TimeAndSpaceComplexity/ConstantTimeAlgorithm.java
@@ -2,14 +2,28 @@ package main.java._01TimeAndSpaceComplexity;
 
 
 public class ConstantTimeAlgorithm {
-    public static void main(String[] args) {
-        int[] arr = {1, 3, 2, 4, 6, 5};
-        System.out.println(arr[2]); //Access element at index 2
-        /*
-          No matter how large array is, accessing an element by index takes constant time O(1).
 
-          Time Complexity: O(1)
-          Space Complexity: O(1)
-         */
+    /*
+    Set of non-recursive and non-loop statements takes O(1) time
+    */
+    public static void swap(int position1, int position2) {
+        int temp = position1;
+        position1 = position2;
+        position2 = temp;
+        System.out.println("Position1: " + position1 + ", " + "Position2: " + position2);
+    }
+
+    /*
+    A loop or recursion that runs a constant number of times is also considered as O(1).
+    */
+    public static void print() {
+        for (int i = 1; i < 3; i++) {
+            System.out.println("Time Complexity analysis is informative."); // C * O(1) = O(1)
+        }
+    }
+
+    public static void main(String[] args) {
+        swap(1, 2);
+        print();
     }
 }
