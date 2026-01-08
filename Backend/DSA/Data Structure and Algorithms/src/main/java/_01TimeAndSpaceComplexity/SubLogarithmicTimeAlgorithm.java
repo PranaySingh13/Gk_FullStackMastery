58104785f68c9415026f785c1c15183d6f7dac8b
PranaySingh13 <pranay.singh.1393@gmail.com>
2026-01-08 21:42:13 +0530

    SubLogarithmicTimeAlgorithm in Time and Space Complexity in DSA
    Signed-off-by: PranaySingh13 <pranay.singh.1393@gmail.com>

---
+++ Backend/DSA/Data Structure and Algorithms/src/main/java/_01TimeAndSpaceComplexity/SubLogarithmicTimeAlgorithm.java
@@ -0,0 +1,16 @@
+package main.java._01TimeAndSpaceComplexity;
+
+public class SubLogarithmicTimeAlgorithm {
+
+    public static void countIterations(double n, double c) {
+        int iterations = 0;
+        for (double i = 2.0; i <= n; i = Math.pow(i, c)) {
+            iterations++;
+        }
+        System.out.println("Total loop runs: " + iterations);
+    }
+
+    public static void main(String[] args) {
+        countIterations(1_000_000_000, 2); //Total loop runs: 5
+    }
+}
